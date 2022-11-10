23863b578e682758ec8dc8ddf6f215d66d2092f7
celia.garcia1984 <celia.garcia@ikasle.egibide.org>
2022-11-10 13:00:20 +0100

    validacion de datos

--- app/js/miPerfil.js
+++ app/js/miPerfil.js
@@ -11,30 +11,34 @@ class Usuario{
 }
 var usu;
 
-
+/* GET ELEMENTOS */
+/*BOTON */
 var boton = document.getElementsByClassName("boton");
 console.log(boton);
 boton[0].addEventListener("click",validar);
+
+/*COMBO*/
 var departamentos = document.getElementById("dept");
 departamentos.addEventListener("change", guardarDept);
 var dept;
+
+/**Esta funcion guarda el departamento en una variable */
 function guardarDept(event){
     dept = event.target.value;
     console.log(dept);
 }
-
+/**Esta funcion valida los datos */
 function validar(){
     try{
         var nombre = document.getElementById("nombre").value;
-        var expRegNombre = new RegExp(/[A-Z][a-z]+/);
         var apellido = document.getElementById("apellido").value;
+        var expRegNombre = new RegExp(/[A-Z][a-z]+/);
+
         var correo = document.getElementById("correo").value;
         var expRegCorreo = new RegExp(/[\w]+@{1}[a-z]+\.[a-z]{2-3}/);
-        
-       
-        
-        
+
         var pass = document.getElementById("pass");
+        var expRegPass = new RegExp(/[0-9]{8}/);
 
         if(nombre == ""){
             alert("El nombre es un campo obligatorio");
@@ -59,12 +63,22 @@ function validar(){
                 throw "El correo no tiene el formato correcto";
             }
         }
-
+        if(pass == ""){
+            alert("La contraseña es un campo obligatorio");
+        }else{
+            if(!expRegPass.test(pass)){
+                throw "La contraseña no tiene el formato correcto";
+            }
+        }
+        crearObjetoUsuario();
 
     }catch(error){
         alert(error);
     }
-    
 
+}
+function crearObjetoUsuario(){
+    usu = new Usuario(nombre,apellido,correo,dept,pass);
+    console.log(usu);
 }
 
