0c30829b417f8a85e0fa73089c1476428c883db8
ikasdev <adrian.rueda@ikasle.egibide.org>
2022-11-11 09:28:24 +0100

    Otra actualizacion para el script SQL

--- reto2Compose/scripts/mysql/script.sql
+++ reto2Compose/scripts/mysql/script.sql
@@ -12,6 +12,13 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
+-- Crear usuario
+-- -----------------------------------------------------
+CREATE USER 'ikasdev'@'%' IDENTIFIED BY 'ACai7925';
+GRANT ALL PRIVILEGES ON db_aergibide.* TO 'ikasdev'@'%';
+FLUSH PRIVILEGES;
+
+-- -----------------------------------------------------
 -- Schema db_aergibide
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `db_aergibide` ;
