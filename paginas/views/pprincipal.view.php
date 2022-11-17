bc520194fcdef8acf0b05c53a319508b47228dd3
aritzcas <aritzcas@gmail.com>
2022-11-17 11:44:24 +0100

    Solucion Main

--- paginas/views/pprincipal.view.php
+++ paginas/views/pprincipal.view.php
@@ -5,43 +5,45 @@ $css = [
 require_once 'parcial/header.php';
 
 ?>
+
 <body>
-    
-<div class="nav">
-    <a href="aaa">Guias</a>
-    <a href="#">Tutoriales</a>
-    <a href="#">Mis Preguntas</a>
-</div>
-<div class="cont-prin">
 
-    <div class="slide">
-        <div class="anadir">
-            <h4>Añadir Pregunta</h4>
-            <!--<input type="submit" id="accion" value="Añadir"> -->
-            <button id='nPregunta'>Añadir</button>
-        </div>
-        <div class="filtros">
-            <h4>Filtros</h4>
+    <div class="nav">
+        <a href="aaa">Guias</a>
+        <a href="#">Tutoriales</a>
+        <a href="#">Mis Preguntas</a>
+    </div>
+    <div class="cont-prin">
 
-            <div class="filtro">
-                <form action="" method="get" class="filtro">
-                <label for="fechInicio">Fecha Inicio:</label>
-                <input type="date" id="fechInicio" name="fechInicio">
-                <br>
-                <label for="fechaFinal">Fecha Final:</label>
-                <input type="date" id="fechaFinal" name="fechaFinal">
-                <br>
-                <label for="tags">Tags:</label>
-                <select name="tags" id="tags">
-                    <option value="">-------------</option>
-                    <option value="mecanica">Mecanica</option>
-                    <option value="electricidad">Electricidad</option>
-                    <option value="electronica">Electronica</option>
-                    <option value="diseño">Diseño</option>
-                </select>
-                <br>
-                <input type="submit" value="Filtar">
-            </form>
+        <div class="slide">
+            <div class="anadir">
+                <h4>Añadir Pregunta</h4>
+                <!--<input type="submit" id="accion" value="Añadir"> -->
+                <button id='nPregunta'>Añadir</button>
+            </div>
+            <div class="filtros">
+                <h4>Filtros</h4>
+
+                <div class="filtro">
+                    <form action="" method="get" class="filtro">
+                        <label for="fechInicio">Fecha Inicio:</label>
+                        <input type="date" id="fechInicio" name="fechInicio">
+                        <br>
+                        <label for="fechaFinal">Fecha Final:</label>
+                        <input type="date" id="fechaFinal" name="fechaFinal">
+                        <br>
+                        <label for="tags">Tags:</label>
+                        <select name="tags" id="tags">
+                            <option value="">-------------</option>
+                            <option value="mecanica">Mecanica</option>
+                            <option value="electricidad">Electricidad</option>
+                            <option value="electronica">Electronica</option>
+                            <option value="diseño">Diseño</option>
+                        </select>
+                        <br>
+                        <input type="submit" value="Filtar">
+                    </form>
+                </div>
             </div>
         </div>
         <div class="main">
@@ -58,6 +60,7 @@ require_once 'parcial/header.php';
         <h4>Preguntas Populares</h4>
     </div>
 
-</div>
-<script src="../js/pprincipal.js"></script>
-<?php require_once 'parcial/footer.php';?>
+    </div>
+    <script src="../js/pprincipal.js"></script>
+    <script src="../js/prueba.js"></script>
+    <?php require_once 'parcial/footer.php'; ?>
