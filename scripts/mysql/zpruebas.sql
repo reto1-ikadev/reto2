593d21bbd7b4fd6f1d554b204222426581e09537
ikasdev <adrian.rueda@ikasle.egibide.org>
2022-11-24 10:44:10 +0100

    Script de pruebas

---
+++ scripts/mysql/zpruebas.sql
@@ -0,0 +1,4 @@
+-- zprueba.sql, la z viene para que se pueda ejecutar después de los comandos de creación
+-- de base de datos y tablas.
+-- Crear un usuario de prueba llamado Roberto.
+INSERT INTO `empleado`  VALUES(90, 'Roberto', 'Prueba', '$2y$10$OUNwNA058r74efK4FXyjKO5u.4s/GzVqJUY32zA9DPjwrbc4M26K2', 'roberto@prueba.com', '1')
