a9d56308082a0f06f8898a9c30e09f19de1488da
ikasdev <adrian.rueda@ikasle.egibide.org>
2022-11-24 12:35:14 +0100

    Correcion comillas dobles

--- scripts/mysql/zpruebas.sql
+++ scripts/mysql/zpruebas.sql
@@ -4,4 +4,4 @@
 INSERT INTO `empleado`  VALUES(90, 'Roberto', 'Prueba', '$2y$10$OUNwNA058r74efK4FXyjKO5u.4s/GzVqJUY32zA9DPjwrbc4M26K2', 'roberto@prueba.com', '1');
 
 -- Crear una pregunta de prueba
-INSERT INTO `pregunta` (`titulo`, `contenido`, `fecha`, `tags`, `empleado_numEmple`) VALUES('¿Que se creo primero el huevo o la gallina?', 'Siempre tuve aquella duda, por ello me gustaria ver si en esta pagina web me podriais sacar esta duda de mi cabeza.', CURDATE(), 'mecanica', 90);
+INSERT INTO `pregunta` (`titulo`, `contenido`, `fecha`, `tags`, `empleado_numEmple`) VALUES("¿Que se creo primero el huevo o la gallina?", "Siempre tuve aquella duda, por ello me gustaria ver si en esta pagina web me podriais sacar esta duda de mi cabeza.", CURDATE(), "mecanica", 90);
