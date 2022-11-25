c38b769d9d2273760f37e64cb3a08bbe960873d9
aritzcas <aritzcas@gmail.com>
2022-11-25 12:05:05 +0100

    Abrir preguntas y respuestas Fav

--- paginas/Db/respuesta_db.php
+++ paginas/Db/respuesta_db.php
@@ -79,7 +79,7 @@ function selectRespuesta($id){
 
 function selectRespuestaIdRespuesta($id){
     $dbh = connect();
-    $stmt = $dbh->prepare("SELECT contenido FROM respuesta WHERE id = :id ORDER BY id DESC LIMIT 1");
+    $stmt = $dbh->prepare("SELECT * FROM respuesta WHERE id = :id ORDER BY id DESC LIMIT 1");
     $stmt->setFetchMode(PDO::FETCH_OBJ);
     $stmt->execute(['id' => $id]);
     
--- paginas/controladores/miPerfil.php
+++ paginas/controladores/miPerfil.php
@@ -85,7 +85,7 @@ include_once "../Db/favoritos_db.php";
             foreach($titulos as $titulo){    
                 $respuesta .= "<div id={$pregunta['pregunta_id']} class=favorito>";
                 $respuesta .= "<div class='pregunta'><h4>{$titulo->titulo}</h4>";
-                $respuesta .= "</div><div><span id={$pregunta['pregunta_id']} name='favP' class='material-symbols-outlined fav'>delete</span>";
+                $respuesta .= "</div><div><a class='ver' href='respuestas.php?id={$pregunta['pregunta_id']}'><span class='material-symbols-outlined'>visibility</span></a>&nbsp;<span id={$pregunta['pregunta_id']} name='favP' class='material-symbols-outlined fav'>delete</span>";
                 $respuesta .= "</div>";
                 $respuesta .= "</div>";
             }
@@ -102,7 +102,7 @@ include_once "../Db/favoritos_db.php";
             foreach($contenidos as $contenido){    
                 $salida .= "<div id={$respuesta['id_respuesta']} class=favorito>";
                 $salida .= "<div class='pregunta'><h4>{$contenido->contenido}</h4>";
-                $salida .= "</div><div><span id={$respuesta['id_respuesta']} name='favR' class='material-symbols-outlined fav'>delete</span>";
+                $salida .= "</div><div><a class='ver' href='respuestas.php?id={$contenido->pregunta_id}'><span class='material-symbols-outlined'>visibility</span></a>&nbsp;<span id={$respuesta['id_respuesta']} name='favR' class='material-symbols-outlined fav'>delete</span>";
                 $salida .= "</div>";
                 $salida .= "</div>";
             }
